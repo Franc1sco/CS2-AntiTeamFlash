f177adbd4322e64df1ff4b1ea2851be704997a4a
Franc1sco Franug <franug13@gmail.com>
2024-01-02 21:05:25 +0100

    Added config for enable, flash owner and adminaccess

--- AntiTeamFlash.cs
+++ AntiTeamFlash.cs
@@ -1,21 +1,34 @@
 ﻿using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes;
+using CounterStrikeSharp.API.Modules.Admin;
+using System.Text.Json.Serialization;
 using Timer = CounterStrikeSharp.API.Modules.Timers.Timer;
 
 namespace AntiTeamFlash;
 
+public class ConfigGen : BasePluginConfig
+{
+    [JsonPropertyName("Enabled")] public bool Enabled { get; set; } = true;
+    [JsonPropertyName("FlashOwner")] public bool FlashOwner { get; set; } = true;
+    [JsonPropertyName("AccessFlag")] public string AccessFlag { get; set; } = "";
+}
 
 [MinimumApiVersion(50)]
-public class AntiTeamFlash : BasePlugin
+public class AntiTeamFlash : BasePlugin, IPluginConfig<ConfigGen>
 {
     public override string ModuleName => "Anti Team Flash";
     public override string ModuleAuthor => "Franc1sco Franug";
-    public override string ModuleVersion => "0.0.1";
+    public override string ModuleVersion => "0.0.2";
+
+    public ConfigGen Config { get; set; } = null!;
+    public void OnConfigParsed(ConfigGen config) { Config = config; }
 
     public override void Load(bool hotReload)
     {
         RegisterEventHandler<EventPlayerBlind>((@event, info) =>
         {
+            if (!Config.Enabled) return HookResult.Continue;
+
             var player = @event.Userid;
             if (!player.IsValid) return HookResult.Continue;
             if (!player.PlayerPawn.IsValid) return HookResult.Continue;
@@ -23,7 +36,9 @@ public class AntiTeamFlash : BasePlugin
             if (!attacker.IsValid) return HookResult.Continue;
             if (!attacker.PlayerPawn.IsValid) return HookResult.Continue;
 
-            if (attacker.PlayerPawn.Value.TeamNum == player.TeamNum)
+            if (Config.AccessFlag != "" && !AdminManager.PlayerHasPermissions(player, Config.AccessFlag)) return HookResult.Continue;
+
+            if (attacker.PlayerPawn.Value.TeamNum == player.TeamNum && (!Config.FlashOwner || attacker.UserId != player.UserId))
             {
                 @event.BlindDuration = 0.1f;
                 return HookResult.Changed;
@@ -34,6 +49,8 @@ public class AntiTeamFlash : BasePlugin
 
         RegisterEventHandler<EventPlayerBlind>((@event, info) =>
         {
+            if (!Config.Enabled) return HookResult.Continue;
+
             var player = @event.Userid;
             if (!player.IsValid) return HookResult.Continue;
             if (!player.PlayerPawn.IsValid) return HookResult.Continue;
@@ -42,7 +59,9 @@ public class AntiTeamFlash : BasePlugin
             if (!attacker.IsValid) return HookResult.Continue;
             if (!attacker.PlayerPawn.IsValid) return HookResult.Continue;
 
-            if (attacker.PlayerPawn.Value.TeamNum == player.TeamNum && (attacker.PlayerPawn.Equals(attacker))
+            if (Config.AccessFlag != "" && !AdminManager.PlayerHasPermissions(player, Config.AccessFlag)) return HookResult.Continue;
+
+            if (attacker.PlayerPawn.Value.TeamNum == player.TeamNum && (!Config.FlashOwner || attacker.UserId != player.UserId))
             {
                 player.PlayerPawn.Value.FlashMaxAlpha = 0.5f;
                 new Timer(player.PlayerPawn.Value.FlashDuration, () =>
