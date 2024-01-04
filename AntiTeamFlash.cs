8261c493f2ad23db63a691e1021337bdc3626781
Franc1sco Franug <franug13@gmail.com>
2024-01-04 17:36:19 +0100

    0.0.3b
    Fixes

--- AntiTeamFlash.cs
+++ AntiTeamFlash.cs
@@ -2,7 +2,6 @@
 using CounterStrikeSharp.API.Core.Attributes;
 using CounterStrikeSharp.API.Modules.Admin;
 using System.Text.Json.Serialization;
-using Timer = CounterStrikeSharp.API.Modules.Timers.Timer;
 
 namespace AntiTeamFlash;
 
@@ -18,7 +17,7 @@ public class AntiTeamFlash : BasePlugin, IPluginConfig<ConfigGen>
 {
     public override string ModuleName => "Anti Team Flash";
     public override string ModuleAuthor => "Franc1sco Franug";
-    public override string ModuleVersion => "0.0.2";
+    public override string ModuleVersion => "0.0.3b";
 
     public ConfigGen Config { get; set; } = null!;
     public void OnConfigParsed(ConfigGen config) { Config = config; }
@@ -40,39 +39,10 @@ public class AntiTeamFlash : BasePlugin, IPluginConfig<ConfigGen>
 
             if (attacker.PlayerPawn.Value.TeamNum == player.TeamNum && (!Config.FlashOwner || attacker.UserId != player.UserId))
             {
-                @event.BlindDuration = 0.1f;
-                return HookResult.Changed;
+                player.PlayerPawn.Value.FlashMaxAlpha = 0.5f;
             }
 
             return HookResult.Continue;
         }, HookMode.Pre);
-
-        RegisterEventHandler<EventPlayerBlind>((@event, info) =>
-        {
-            if (!Config.Enabled) return HookResult.Continue;
-
-            var player = @event.Userid;
-            if (!player.IsValid) return HookResult.Continue;
-            if (!player.PlayerPawn.IsValid) return HookResult.Continue;
-
-            var attacker = @event.Attacker;
-            if (!attacker.IsValid) return HookResult.Continue;
-            if (!attacker.PlayerPawn.IsValid) return HookResult.Continue;
-
-            if (Config.AccessFlag != "" && !AdminManager.PlayerHasPermissions(player, Config.AccessFlag)) return HookResult.Continue;
-
-            if (attacker.PlayerPawn.Value.TeamNum == player.TeamNum && (!Config.FlashOwner || attacker.UserId != player.UserId))
-            {
-                player.PlayerPawn.Value.FlashMaxAlpha = 0.5f;
-                new Timer(player.PlayerPawn.Value.FlashDuration, () =>
-                {
-                    if (!player.IsValid) return;
-                    if (!player.PlayerPawn.IsValid) return;
-                    player.PlayerPawn.Value.FlashMaxAlpha = 0.5f;
-                });
-            }
-
-            return HookResult.Continue;
-        });
     }
 }
